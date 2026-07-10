38227cf64b3588355ecd9739578fcebb49a30375
koyakato0118 <katou.kouya.0118@gmail.com>
2026-07-10 12:47:42 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+
+function toggleSidebar(){document.getElementById('sidebar').classList.toggle('open');}
+function closeSidebar(){if(window.innerWidth<=900)document.getElementById('sidebar').classList.remove('open');}
